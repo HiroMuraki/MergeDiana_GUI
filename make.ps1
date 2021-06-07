8b61870c3fc77c1a298e9eae85d5a458d8267631
HiroMuraki <419734782@qq.com>
2021-06-07 15:09:53 +0800

    update build script

--- make.ps1
+++ make.ps1
@@ -1,2 +1,6 @@
 dotnet publish -r win10-x64 -c release --self-contained=False /p:PublishSingleFile=True -o "OutputProgram"
-ren "OutputProgram\MergeDiana_GUI.exe" "合成然然.exe"
+$hasPath = Test-Path "OutputProgram\合成然然.exe"
+if ($hasPath) { 
+    Move-Item -Force "OutputProgram\合成然然.exe" "OutputProgram\新建文件夹\合成然然.exe"
+}
+Rename-Item "OutputProgram\MergeDiana_GUI.exe" "合成然然.exe"
